f0763dff6af56b753a9b95d613e96de440c9f743
Hoon Wee <mrgravity817@gmail.com>
2023-08-06 10:38:37 +0900

    Initial Commit

---
+++ SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by Hoon Wee on 2023/08/06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
