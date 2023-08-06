6d141f8934d752889c9a4813781deed2c511929b
Hoon Wee <mrgravity817@gmail.com>
2023-08-06 18:13:25 +0900

    fix: do not use binding for readonly

--- SimpleWeather/ContentView.swift
+++ SimpleWeather/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            BackgroundView(isNight: $isNight)
+            BackgroundView(isNight: isNight)
             
             VStack {
                 CityTextView(cityName: "Cupertino, CA")
@@ -96,7 +96,7 @@ struct WeatherDayView: View {
 }
 
 struct BackgroundView: View {
-    @Binding var isNight: Bool
+    var isNight: Bool
     
     var body: some View {
         LinearGradient(
@@ -104,7 +104,7 @@ struct BackgroundView: View {
                 colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing)
-        .edgesIgnoringSafeArea(.all)
+            .ignoresSafeArea()
     }
 }
 
