7b30554eab085d4c1f1b213b4ed5e97ccfa259e8
Hoon Wee <mrgravity817@gmail.com>
2023-08-06 18:06:37 +0900

    refactor: bind the dark mode into background-view

--- SimpleWeather/ContentView.swift
+++ SimpleWeather/ContentView.swift
@@ -13,9 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            BackgroundView(
-                topColor: isNight ? .black : .blue,
-                bottomColor: isNight ? .gray : Color("lightBlue"))
+            BackgroundView(isNight: $isNight)
             
             VStack {
                 CityTextView(cityName: "Cupertino, CA")
@@ -98,14 +96,14 @@ struct WeatherDayView: View {
 }
 
 struct BackgroundView: View {
-    var topColor: Color
-    var bottomColor: Color
+    @Binding var isNight: Bool
     
     var body: some View {
         LinearGradient(
-            gradient: Gradient(colors: [topColor, bottomColor]),
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing)
+            gradient: Gradient(
+                colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing)
         .edgesIgnoringSafeArea(.all)
     }
 }
