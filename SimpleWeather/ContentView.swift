936a4295f18079ab2cf79532e7f0747536533a32
Hoon Wee <mrgravity817@gmail.com>
2023-08-06 17:12:50 +0900

    feat: add change button

--- SimpleWeather/ContentView.swift
+++ SimpleWeather/ContentView.swift
@@ -59,6 +59,20 @@ struct ContentView: View {
                 }
                 
                 Spacer()
+                
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
+                
+                
             }
             
         }
