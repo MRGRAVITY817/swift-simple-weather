39f0dd6ae8202ed3f3e64daa698af9df37424e83
Hoon Wee <mrgravity817@gmail.com>
2023-08-06 17:26:24 +0900

    refactor: extract button

--- SimpleWeather/ContentView.swift
+++ SimpleWeather/ContentView.swift
@@ -45,11 +45,10 @@ struct ContentView: View {
                 Button {
                     print("tapped")
                 } label: {
-                    Text("Change Day Time")
-                        .frame(width: 280, height: 50)
-                        .background(Color.white)
-                        .font(.system(size: 20, weight: .bold, design: .default))
-                        .cornerRadius(10)
+                    WeatherButton(
+                        title: "Change Day Time",
+                        textColor: .blue,
+                        backgroundColor: .white)
                 }
                 
                 Spacer()
@@ -134,5 +133,21 @@ struct MainWeatherStatusView: View {
                 .foregroundColor(.white)
         }
         .padding(.bottom, 40)
+    }
+}
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+         Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
     }
 }
